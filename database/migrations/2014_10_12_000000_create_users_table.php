9754930f999e54d882a8b34e6b0d5acb99c38d88
uu <19240415@bsi.ac.id>
2026-01-12 13:20:08 +0700

    menambahkan function di user_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->enum('role', ['admin', 'customer'])->default('customer');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();  // TAMBAHAN: Nomor telepon
+            $table->enum('role', ['admin', 'user'])->default('user');  // TAMBAHAN: Role user
             $table->rememberToken();
             $table->timestamps();
         });
