ba129c946d9af9c1a4accaa45d606492fd6c0d7b
uu <19240415@bsi.ac.id>
2026-01-15 08:51:07 +0700

    menambahkan interaksi

--- resources/views/components/stats-card.blade.php
+++ resources/views/components/stats-card.blade.php
@@ -1,16 +1,18 @@
-@props(['icon', 'title', 'value', 'color' => 'blue', 'trend' => null])
+@props(['icon' => null, 'title' => '', 'value' => 0, 'color' => 'blue', 'trend' => null])
 
 <div class="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow">
     <div class="flex items-center justify-between">
-        <div>
+        <div class="flex-1">
             <p class="text-sm font-medium text-gray-600 mb-1">{{ $title }}</p>
             <p class="text-3xl font-bold text-gray-800">{{ $value }}</p>
             @if($trend)
                 <p class="text-sm text-{{ $color }}-600 mt-2">{{ $trend }}</p>
             @endif
         </div>
+        @if($icon)
         <div class="w-14 h-14 bg-{{ $color }}-100 rounded-full flex items-center justify-center">
             {!! $icon !!}
         </div>
+        @endif
     </div>
 </div>
